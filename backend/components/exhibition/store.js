8287e4c69e2e0542f85d07ada308d9024bda6c21
David Burbano <burbano.renato@gmail.com>
2020-09-27 22:25:14 -0500

    Videos

--- backend/components/exhibition/store.js
+++ backend/components/exhibition/store.js
@@ -27,7 +27,7 @@ function getAllValidYears(){
 
     return new Promise((resolve, reject) =>{
 
-        ModelExhibition.find()
+        ModelExhibition.find({})
             .then((fairs) => {
                 console.log("Fairs: ", fairs.length)
                 var years = []
@@ -51,24 +51,10 @@ function getAllValidYears(){
 function getExhibitions(){
     return new Promise((resolve, reject) =>{
 
-        ModelExhibition.find()
-            .then((fairs) => {
-
-                console.log("Fairs: ", fairs.length)
-
-                var years = []
-
-                for(var i=0; i< fairs.length; i++){
-                  const year = fairs[i].date.substring(0,4)
-                  console.log(year)
-                  if(!years.includes(year)){
-                    years.push(year)
-                  }
-                }
-
-                resolve({status: 1, data: years.sort().reverse(),  msg: "Años consultados"})  // status 1= todo correcto
-
-
+        ModelExhibition.find({ is_fair: false })
+            .then((exhibitions) => {
+                
+                resolve({status: 1, data: exhibitions,  msg: "Años consultados"})  // status 1= todo correcto
             })
             .catch(e=>{
                 console.log(e)
